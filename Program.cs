46c54680f98f6570421671bcdcfb6596f155b9da
Abakaxato <cauakika@gmail.com>
2026-05-16 17:47:30 -0300

    Andamento da aula 19

--- Program.cs
+++ Program.cs
@@ -6,36 +6,23 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
+            //Declaracoes de variaveis
             bool estado = true;
             char genero = 'f';
             char genero2 = '\u0046';
-            byte n1 = 126;
+            byte n1 = 23;
             int n2 = 1000;
             int n3 = 2147483647;
             long n4 = 21474836488;
             float n5 = 1.1f;
-            double n6 = 1.1;
+            double n6 = 44.1352132;
             string n7 = "caua";
-            object n8 = "caua objeto";
+            object n8 = "objeto caua";
             int n9 = int.MinValue;
             int n10 = int.MaxValue;
             decimal n11 = decimal.MaxValue;
 
-            Console.WriteLine(estado);
-            Console.WriteLine(genero);
-            Console.WriteLine(genero2);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4); 
-            Console.WriteLine(n5); 
-            Console.WriteLine(n6); 
-            Console.WriteLine(n7); 
-            Console.WriteLine(n8); 
-            Console.WriteLine(n9); 
-            Console.WriteLine(n10); 
-            Console.WriteLine(n11); 
-
+            Console.WriteLine(n7 + " tem " + n1 + " anos e pesa " + n6.ToString("F2") + " kilos"); 
         }
     }
 }
