3cbec46e61cab82bb09ee3240f085a69a957c00a
Abakaxato <cauakika@gmail.com>
2026-05-16 17:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool estado = true;
+            char genero = 'f';
+            char genero2 = '\u0046';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 21474836488;
+            float n5 = 1.1f;
+            double n6 = 1.1;
+            string n7 = "caua";
+            object n8 = "caua objeto";
+
+            Console.WriteLine(estado);
+            Console.WriteLine(genero);
+            Console.WriteLine(genero2);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4); 
+            Console.WriteLine(n5); 
+            Console.WriteLine(n6); 
+            Console.WriteLine(n7); 
+            Console.WriteLine(n8); 
+
+
+        }
+    }
+}
